402541a3fc2656ba0a24182f89c703d1f0afdcb6
PurvaBhilare <47863521+PurvaBhilare@users.noreply.github.com>
2024-09-27 09:07:32 +0100

    final update

--- euler1.js
+++ euler1.js
@@ -1,7 +1,7 @@
 let sum = 0
-for(let i=1;i<=5;i++){
+for(let i=1;i<1000;i++){
     if(i%3==0 || i%5==0){
         sum += i
     }
 }
-console.log(sum)
+console.log(`Sum of mulyiples of 3 or 5 below 100 is ${sum}`)
