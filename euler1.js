c56f45305fbb722689abf5c9347938fc0c48458f
PurvaBhilare <47863521+PurvaBhilare@users.noreply.github.com>
2024-09-27 09:51:11 +0100

    commented

--- euler1.js
+++ euler1.js
@@ -1,10 +1,10 @@
-let sum = 0
-for(let i=1;i<1000;i++){
-    if(i%3==0 || i%5==0){
-        sum += i
-    }
-}
-console.log(`Sum of mulyiples of 3 or 5 below 1000 is ${sum}`)
+// let sum = 0
+// for(let i=1;i<1000;i++){
+//     if(i%3==0 || i%5==0){
+//         sum += i
+//     }
+// }
+// console.log(`Sum of mulyiples of 3 or 5 below 1000 is ${sum}`)
 
 // Create a functn to find the sum of all the multiples of and b where s,b,n are function parameters
 let sm = 0
