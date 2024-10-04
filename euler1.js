0d353d526348da58a594c468809568e1cfcbcdac
PurvaBhilare <47863521+PurvaBhilare@users.noreply.github.com>
2024-10-04 09:18:37 +0100

    changed code

--- euler1.js
+++ euler1.js
@@ -7,8 +7,9 @@
 // console.log(`Sum of mulyiples of 3 or 5 below 1000 is ${sum}`)
 
 // Create a functn to find the sum of all the multiples of and b where s,b,n are function parameters
-let sm = 0
+
 sumOfMultiples = (a,b,n) =>{
+    let sm = 0
     for(let i=1;i<n;i++){
         if(i%a==0 || i%b==0){
             sm += i
